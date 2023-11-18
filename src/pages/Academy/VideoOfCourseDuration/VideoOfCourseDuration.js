c7b6a07b6ee31a5f27858f20d421fcdd90d1e55a
DevWaleedS <eng.waleed.salah@outlook.com>
2023-11-18 12:25:21 +0200

    commit

--- src/pages/Academy/VideoOfCourseDuration/VideoOfCourseDuration.js
+++ src/pages/Academy/VideoOfCourseDuration/VideoOfCourseDuration.js
@@ -1,16 +1,45 @@
-import react from "react";
+import React from "react";
 
 const VideoOfCourseDuration = ({ duration }) => {
-	// To check the hour ,secondes and minutes
-	const [hours, minutes] = duration?.split(":");
+	// To check the hour, minutes, and seconds
+	const [hours, minutes, seconds] = duration?.split(":");
 
-	// Display hours, and minutes
-	return (
-		<>
-			{parseInt(hours, 10) > 0 && `${hours} ساعة`}
-			{parseInt(minutes, 10) > 0 && `${minutes} دقيقة `}
-		</>
-	);
+	// Convert hours, minutes, and seconds values to integers
+	const hoursValue = parseInt(hours, 10);
+	const minutesValue = parseInt(minutes, 10);
+	const secondsValue = parseInt(seconds, 10);
+
+	// Generate the formatted duration string
+	let formattedDuration = "";
+
+	// Handle hours
+	if (hoursValue > 0) {
+		formattedDuration += `${
+			hoursValue === 1
+				? "ساعة"
+				: hoursValue === 2
+				? "ساعتين"
+				: hoursValue <= 10
+				? `ساعات ${hoursValue}`
+				: `ساعة ${hoursValue}`
+		} `;
+	}
+
+	// Handle minutes
+	if (minutesValue > 0) {
+		formattedDuration += `${formattedDuration ? "و " : ""} ${
+			minutesValue <= 9 ? `${minutesValue} دقائق` : `${minutesValue} دقيقة`
+		}  `;
+	}
+
+	// Handle seconds
+	if (secondsValue > 0) {
+		formattedDuration += `${formattedDuration ? "و " : ""}${
+			secondsValue <= 10 ? `${secondsValue} ثواني` : `${secondsValue} ثانية`
+		}  `;
+	}
+
+	return <>{formattedDuration}</>;
 };
 
 export default VideoOfCourseDuration;
