412f094ada857f3f1fce7fe9eabd679437e9a953
anwarramadan <anwarramadan@inetwork-me.com>
2024-05-16 18:29:39 +0300

    set and get localstorage

--- src/pages/Authentication/Login/PrivateRoute/PrivateRoute.jsx
+++ src/pages/Authentication/Login/PrivateRoute/PrivateRoute.jsx
@@ -4,10 +4,7 @@ const PrivateRoute = ({ children }) => {
 	const location = useLocation();
 
 	// Simplified token retrieval from cookies only.
-	const store_token = document.cookie
-		.split("; ")
-		.find((cookie) => cookie.startsWith("store_token="))
-		?.split("=")[1];
+	const store_token = localStorage.getItem("store_token");
 
 	// Redirect if no token is found. No need to modify the cookie here.
 	return store_token ? (
